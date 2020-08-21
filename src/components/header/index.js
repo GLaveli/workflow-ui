d93eb8d456758fd80a5093a12c8638bfd9d9f97c
unknown <g.laveli.p@gmail.com>
2020-08-21 08:43:00 -0300

    up

---
+++ src/components/header/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import certImage from '../../assets/certi.png';
+
+import './style.css';
+export default function Header() {
+
+ return (
+  <div className="header_container">
+   <input type="checkbox" id="check" />
+
+   <header>
+    <label for="check" >\
+    <i class="fas fa-bars" id="sidebar_btn"></i>
+    </label>
+    <div className="left_area">
+     <h3> Node-<span>red</span></h3>
+    </div>
+    <div className="rig_area">
+     <a href=" " className="logout_btn">Logout</a>
+    </div>
+   </header>
+
+   <div className="sidebar">
+    <center>
+     <img src={certImage} className="menu_image" alt="imgTopo" />
+     <hr />
+     <span className="menuTitle">Menu</span>
+    </center>
+    <div className="itemContainer">
+     <a href=" " ><i className="fas fa-desktop"></i><span>Menu 01</span></a>
+     <a href=" " ><i className="fas fa-cogs"></i><span>Menu 01</span></a>
+     <a href=" " ><i className="fas fa-table"></i><span>Menu 01</span></a>
+     <a href=" " ><i className="fas fa-th"></i><span>Menu 01</span></a>
+     <a href=" " ><i className="fas fa-info-circle"></i><span>Menu 01</span></a>
+     <a href=" " ><i className="fas fa-sliders-h"></i><span>Menu 01</span></a>
+    </div>
+   </div>
+  </div>
+ )
+};
---
+++ src/pages/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+import Header from '../../components/header';
+
+export default function Home() {
+
+ return (
+  <>
+   <Header />
+   <div className="content">
+    
+   </div>
+  </>
+ )
+};
